2fc2b1a47e1714199e6396e33189be2f74e402ed
張之耀(Chih-Yao, Chang) RTK <m900054@gmail.com>
2024-03-10 01:48:54 +0800

    Update main.py

--- resize_small_image/main.py
+++ resize_small_image/main.py
@@ -1,6 +1,40 @@
 from PIL import Image
-w = 24
-h = 24
-im = Image.open("shuttle.png")
-im = im.resize((w, h), resample=Image.LANCZOS)
-im.save("output.png")
+
+INPUT_IMG_NAME = 'ex_1.png'
+OUTPUT_IMG_NAME = 'output.png'
+
+def resize_to_fixed_w_h():
+    w = 24                                  # fixed
+    h = 24                                  # fixed
+    img = Image.open(INPUT_IMG_NAME)
+    img = img.resize((w, h), resample=Image.LANCZOS)
+    img.save(OUTPUT_IMG_NAME)
+
+def resize_by_scale():
+    scale_percentage = 0.5
+    img = Image.open(INPUT_IMG_NAME)
+    width, height = img.size
+    w = int(width * scale_percentage)
+    h = int(height * scale_percentage)
+    img = img.resize((w, h), resample=Image.LANCZOS)
+    img.save(OUTPUT_IMG_NAME)
+
+def resize_by_w_same_ratio():
+    img = Image.open(INPUT_IMG_NAME)
+    width, height = img.size
+    w = 600                                 # fixed
+    scale_percentage = w / width
+    h = int(height * scale_percentage)
+    img = img.resize((w, h), resample=Image.LANCZOS)
+    img.save(OUTPUT_IMG_NAME)
+
+def resize_by_h_same_ratio():
+    img = Image.open(INPUT_IMG_NAME)
+    width, height = img.size
+    h = 750                                 # fixed
+    scale_percentage = h / height
+    w = int(width * scale_percentage)
+    img = img.resize((w, h), resample=Image.LANCZOS)
+    img.save(OUTPUT_IMG_NAME)
+
+resize_by_w_same_ratio()
